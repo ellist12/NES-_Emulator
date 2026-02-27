911a01dac564db8ea25021603479807838719092
ellist12 <ellist1212@gmail.com>
2026-02-28 02:19:15 +0700

    implement opcode 10

--- src/cpu.rs
+++ src/cpu.rs
@@ -81,6 +81,33 @@ impl Cpu {
 
         //2. DECODE & EXECUTE: Cek opcodenya apa
         match opcode {
+            0x10 => {
+                // BPL (Branch if positive)
+                // Melompat ke baris kode lain jika hasil operasi sebelumnya positif, jumlah lompatan tergantung dengan 1 byte berikutnya
+                // Untuk BNE, angka di 1 byte berikutnya harus kita konversi dulu menjadi signed integer i8 sebelum kita operasikan
+                // Ukuran Opcode : 2 byte
+                // Jumlah cycle :
+                //      1. 2 jika kondisi tidak terpenuhi
+                //      2. 3 jika kondisi terpenuhi dan tidak melewati *page boundary*
+                //      3. 4 jika kondisi terpenuhi dan melewati *page_boundary*
+                // Contoh kode assembly : BPL $05
+                // Artinya : Jika bit flag negatif di register status == 0, lompat 5 byte kedepan
+                let mut cycle = 2;
+                let bytes_to_jump = bus.read(self.pc);
+                self.pc += 1;
+                if self.status & 0b10000000 == 0 {
+                    cycle += 1;
+                    let offset = bytes_to_jump as i8;
+                    let old_pc = self.pc;
+                    let new_pc = self.pc.wrapping_add_signed(offset as i16);
+                    if (old_pc & 0xFF00) != (new_pc & 0xFF00) {
+                        cycle += 1;
+                    }
+                    self.pc = new_pc;
+                }
+                self.cycle += cycle;
+                cycle
+            }
             0x78 => {
                 // SEI (Set Interrupt Flag)
                 // Nyalakan bit flag interrupt di status (0b00000100)
