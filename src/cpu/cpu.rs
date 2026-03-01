ce46aec658a76317589d2ed9c6525ea6658fe0ab
ellist12 <ellist1212@gmail.com>
2026-03-01 14:18:39 +0700

    move sta indirect_y instruction to its own struct

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -123,30 +123,7 @@ impl Cpu {
                 DEY::decrease(self)
             }
             0x91 => {
-                // STA (Indirect), Y : Lihat angka di alamat ram ZEROPAGE yang ditunjuk oleh byte berikutnya,
-                //                     baca 2 byte dari alamat itu untuk mendapatkan sebuah alamat baru,
-                //                     alamat baru itu kemudian tambahkan dengan value yang ada di register Y
-                //                     simpan nilai register A ke alamat baru yang sudah ditambahkan dengan value
-                //                     di register Y tersebut
-                // Ukuran Opcode : 2 byte
-                // Jumlah cycle : 6
-                // Contoh kode assembly : STA ($20), Y
-                // Artinya :
-                //   1. Lihat angka yang ditunjuk oleh byte berikutnya, misal $20
-                //   2. Baca value dari byte di alamat $20 dan $21, lalu gabungkan jadi satu alamat di satu variabel u16 baru, misal $8000
-                //   3. Ambil nilai di register Y, misal $05
-                //   4. Jumlahkan keduanya, $8000 + $0005 = $8005
-                //   5. Tulis value di register A ke alamat $8005
-                let param = bus.read(self.pc) as u16;
-                self.pc = self.pc.wrapping_add(1);
-                let lo = bus.read(param) as u16;
-                let hi = bus.read(param + 1) as u16;
-                let addr_to_add = (hi << 8) | lo;
-                let addr = addr_to_add + self.y as u16;
-                println!("STA (${}), Y", param);
-                bus.write(addr, self.a);
-                self.cycle += 6;
-                6
+                STA::indirect_y(self, bus)
             }
             0x9A => {
                 // TXS: Transfer X to Stack Pointer
--- src/cpu/instructions/sta.rs
+++ src/cpu/instructions/sta.rs
@@ -36,4 +36,31 @@ impl STA {
         cpu.cycle += 4;
         4
     }
+
+    // STA (Indirect), Y : Lihat angka di alamat ram ZEROPAGE yang ditunjuk oleh byte berikutnya,
+    //                     baca 2 byte dari alamat itu untuk mendapatkan sebuah alamat baru,
+    //                     alamat baru itu kemudian tambahkan dengan value yang ada di register Y
+    //                     simpan nilai register A ke alamat baru yang sudah ditambahkan dengan value
+    //                     di register Y tersebut
+    // Ukuran Opcode : 2 byte
+    // Jumlah cycle : 6
+    // Contoh kode assembly : STA ($20), Y
+    // Artinya :
+    //   1. Lihat angka yang ditunjuk oleh byte berikutnya, misal $20
+    //   2. Baca value dari byte di alamat $20 dan $21, lalu gabungkan jadi satu alamat di satu variabel u16 baru, misal $8000
+    //   3. Ambil nilai di register Y, misal $05
+    //   4. Jumlahkan keduanya, $8000 + $0005 = $8005
+    //   5. Tulis value di register A ke alamat $8005
+    pub fn indirect_y(cpu: &mut Cpu, bus: &mut Bus) -> u16 {
+        let param = bus.read(cpu.pc) as u16;
+        cpu.pc = cpu.pc.wrapping_add(1);
+        let lo = bus.read(param) as u16;
+        let hi = bus.read(param + 1) as u16;
+        let addr_to_add = (hi << 8) | lo;
+        let addr = addr_to_add + cpu.y as u16;
+        println!("STA (${}), Y", param);
+        bus.write(addr, cpu.a);
+        cpu.cycle += 6;
+        6
+    }
 }
